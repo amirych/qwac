92af937b7dcf79e0163faf28e34417525cdd9606
Timur A. Fatkhullin <amirych@gmail.com>
2016-04-18 18:07:01 +0300

    ...

--- focussingplotdialog.cpp
+++ focussingplotdialog.cpp
@@ -1,7 +1,46 @@
 #include "focussingplotdialog.h"
 
-FocussingPlotDialog::FocussingPlotDialog(QWidget *parent): QDialog(parent)
+#include "qwt_plot_curve.h"
+#include <qwt_symbol.h>
+#include <cmath>
+
+FocussingPlotDialog::FocussingPlotDialog(const QString &title, QWidget *parent): QDialog(parent)
 {
     ui.setupUi(this);
 
+    ui.focusCurvePlot->setTitle(title);
+    ui.focusCurvePlot->setAxisTitle(QwtPlot::yLeft,"FWHM");
+    ui.focusCurvePlot->setAxisTitle(QwtPlot::xBottom,"Focus value");
+}
+
+
+void FocussingPlotDialog::plot(QVector<double> &foc_value, QVector<double> &fwhm,
+                               QVector<double> &fit_coeffs, QList<QPixmap> &images)
+{
+
+
+    // measured point
+    QwtPlotCurve *data_curve = new QwtPlotCurve;
+    data_curve->setStyle(QwtPlotCurve::NoCurve);
+    data_curve->setSymbol(QwtSymbol::Diamond);
+
+    data_curve->setSamples(foc_value,fwhm);
+
+    data_curve->attach(ui.focusCurvePlot);
+
+    // fit curve (polynom)
+    // coeffs: c0 + c1*x + c2*x^2 + ...
+
+    QwtPlotCurve *fit_curve = new QwtPlotCurve;
+    fit_curve->setStyle(QwtPlotCurve::Lines);
+
+    data_curve->setSymbol(QwtSymbol::NoSymbol);
+
+    QVector<double> fit_fwhm(fwhm.size(),fit_coeffs[0]);
+
+    for ( size_t order = 1; order < fit_coeffs.size(); ++order ) {
+        for ( size_t i = 0; i < foc_value.size(); ++i ) {
+            fit_fwhm[i] += fit_coeffs[order]*std::pow(foc_value[i],order);
+        }
+    }
 }
--- focussingplotdialog.h
+++ focussingplotdialog.h
@@ -2,17 +2,24 @@
 #define FOCUSSINGPLOTDIALOG_H
 
 #include <QDialog>
-
+#include <QList>
+#include <QPixmap>
+#include <QVector>
+#include <QString>
 
 #include "ui_focussingCurvePlot.h"
 
 class FocussingPlotDialog : public QDialog
 {
 public:
-    FocussingPlotDialog(QWidget *parent = nullptr);
+    FocussingPlotDialog(const QString &title=QString::null, QWidget *parent = nullptr);
+
+    void plot(QVector<double> &foc_value, QVector<double> &fwhm,
+              QVector<double> &fit_coeffs, QList<QPixmap> &images);
 
 private:
     Ui::FocussingPlotForm ui;
+
 };
 
 #endif // FOCUSSINGPLOTDIALOG_H
